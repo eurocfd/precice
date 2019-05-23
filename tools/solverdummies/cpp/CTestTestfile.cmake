f194a3c71229061158e36bbf5ca50fda96e46d84
BenjaminRueth <benjamin.rueth@tum.de>
2019-05-23 15:32:57 +0200

    adds test case for read/writeScalarData in CPP solverdummy

---
+++ tools/solverdummies/cpp/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/benjamin/precice/tools/solverdummies/cpp
+# Build directory: /home/benjamin/precice/tools/solverdummies/cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(precice.solverdummy "/usr/bin/cmake" "-DDUMMY_EXE=/home/benjamin/precice/tools/solverdummies/cpp/solverdummy" "-DDUMMY_CONFIG=/home/benjamin/precice/tools/solverdummies/cpp/../precice-config.xml" "-V" "-P" "/home/benjamin/precice/tools/solverdummies/cpp/test.cmake")
--- tools/solverdummies/cpp/solverdummy.cpp
+++ tools/solverdummies/cpp/solverdummy.cpp
@@ -15,19 +15,23 @@ int main (int argc, char **argv)
   using namespace precice;
   using namespace precice::constants;
 
-  if (argc != 4){
+  if (argc != 6){
     std::cout << "Usage: ./solverdummy configFile solverName meshName\n";
     std::cout << '\n';
     std::cout << "Parameter description\n";
     std::cout << "  configurationFile: Path and filename of preCICE configuration\n";
     std::cout << "  solverName:        SolverDummy participant name in preCICE configuration\n";
     std::cout << "  meshName:          Mesh in preCICE configuration that carries read and write data\n";
+    std::cout << "  writeDataName:          Name of write data\n";
+    std::cout << "  readDataName:          Name of read data\n";
     return 1;
 }
 
   std::string configFileName(argv[1]);
   std::string solverName(argv[2]);
   std::string meshName(argv[3]);
+  std::string writeDataName(argv[4]);
+  std::string readDataName(argv[5]);
   int N = 1;
 
   SolverInterface interface(solverName, commRank, commSize);
@@ -36,13 +40,18 @@ int main (int argc, char **argv)
   int meshID = interface.getMeshID(meshName);
   int dimensions = interface.getDimensions();
   std::vector<std::vector<double>> vertices(N, std::vector<double>(dimensions, 0)); 
-  std::vector<int> dataIndices(N,0);
+  std::vector<int> valueIndices(N,0);
+  int writeDataID = interface.getDataID(writeDataName, meshID);
+  int readDataID = interface.getDataID(readDataName, meshID);
 
   for(int i=0; i<N; i++){
-    dataIndices[i] = interface.setMeshVertex(meshID, vertices[i].data());
+    valueIndices[i] = interface.setMeshVertex(meshID, vertices[i].data());
   }
 
   double dt = interface.initialize();
+
+  double readValue = -1;
+  double writeValue = 5;
 
   while (interface.isCouplingOngoing()){
 
@@ -51,7 +60,17 @@ int main (int argc, char **argv)
       interface.fulfilledAction(actionWriteIterationCheckpoint());
     }
 
+    std::cout << "before\n";
+    std::cout << "readValue:" << readValue << "\n";
+    std::cout << "writeValue:" << writeValue << "\n";
+
+    interface.writeScalarData(writeDataID, valueIndices[0], writeValue);
     dt = interface.advance(dt);
+    interface.readScalarData(readDataID, valueIndices[0], readValue);
+
+    std::cout << "after\n";
+    std::cout << "readValue:" << readValue << "\n";
+    std::cout << "writeValue:" << writeValue << "\n";
 
     if (interface.isActionRequired(actionReadIterationCheckpoint())){
       std::cout << "DUMMY: Writing iteration checkpoint\n";
